571c1fb0e299eeb06c50c4d1a126414e837a48e1
yuma itonaga <yumayuma0322@gmail.com>
2021-12-19 11:39:44 +0900

    udpate

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -4,7 +4,7 @@ export default class Create extends React.Component {
   render(){
     return (
       <main style={{ padding: "1rem 0" }}>
-        <h2>Expenses</h2>
+        <h2>Expense s</h2>
       </main>
     );
   }
